1ddb79f25fd33e9ae6c738c6e47afa64fcce5b6b
Ethan Pinto <ethanpintoa@gmail.com>
2025-10-24 16:56:56 -0400

    Add frontend UI

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
 
 from model import model
+from templates import templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def main_page(request: Request):
+    return templates.TemplateResponse("main.html.jinja", {"request": request})
 
 
 @app.post("/call-model", response_class=JSONResponse)
---
+++ static/scripts/api.js
@@ -0,0 +1,23 @@
+//@ts-check
+
+/** @import { User } from './context.js' */
+
+/**
+ *
+ * @param {User[]} users - Array of User availability data.
+ * @returns {Promise<Object>} - The response from the server.
+ */
+async function submitAvailability(users) {
+  const availability = users.map((user) => ({ name: user.name, timeSlots: user.timeSlots }));
+  const response = await fetch("/call-model", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ availability }),
+  });
+
+  if (!response.ok) throw new Error(`Server error: ${response.statusText}`);
+
+  return await response.json();
+}
+
+export { submitAvailability };
